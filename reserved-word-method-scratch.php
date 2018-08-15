20c855a21a20d17253ecc9f3403314e388e9614d
Fenton Martin <fentonxm@gmail.com>
2018-08-15 22:08:27 +0700

    Update method scratch

--- reserved-word-method-scratch.php
+++ reserved-word-method-scratch.php
@@ -3,24 +3,24 @@
 $str = "setyagus   sucipto   sedang makan";
 // Print string
 echo "<pre>".$str."<pre>";
-// Scratch method
+
+// SCRATCH METHOD
 // ----------------------------------------
 // Split string
 $splitstr = str_split($str);
-
 // Print string
 $str="";
+// Set number
 $n = 0;
-
+// Loop for reverse
 for ($x = sizeof($splitstr)-1; $x >= 0; $x--) {
-  $n++;
-  echo "</br>Step $n : ";
-  $str=$str.$splitstr[$x];
-echo "<pre>".$str."<pre>";
+	$n++;
+  	echo "</br>Step $n : ";
+  	$str=$str.$splitstr[$x];
+	echo "<pre>".$str."<pre>";
 }
+// ----------------------------------------
 
-// $str = str_replace("%_%"," ",$str);
 // Print string
-
 echo "<pre>".$str."<pre></br>";
 ?>
