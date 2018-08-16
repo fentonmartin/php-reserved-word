cad111aa81aa97f6b94c0853fa4b5f7ff17ac518
Fenton Martin <fentonxm@gmail.com>
2018-08-16 15:49:25 +0700

    Update Method v2

--- reserved-word-method-scratch.php
+++ reserved-word-method-scratch.php
@@ -19,6 +19,26 @@ for ($x = sizeof($splitstr)-1; $x >= 0; $x--) {
   	$str=$str.$splitstr[$x];
 	echo "<pre>".$str."<pre>";
 }
+// SCRATCH METHOD v2
+// ----------------------------------------
+// Split string
+$splitstr = str_split($str);
+// Print string
+$str="";
+// Set number
+$n = 0;
+// Set array
+$arr = array();
+// Loop for reverse
+for ($x = 0; $x <= sizeof($splitstr); $x+) {
+  	if ($splitstr[$x]==" ") {
+  		$array[$n]=$splitstr[$x];
+  	}
+  	else {
+  		$str=$str.$splitstr[$x];	
+  	}
+  	echo "<pre>".$str."<pre>";
+}
 // ----------------------------------------
 
 // Print string
